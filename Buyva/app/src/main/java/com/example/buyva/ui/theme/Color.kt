0ca98b8dc04908978b3011a5e0b8f41195daea3e
Nouragami7 <https://github.com/Nouragami7>
2025-06-12 14:40:28 +0300

    app structure

---
+++ Buyva/app/src/main/java/com/example/buyva/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.buyva.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Gray = Color(0xFFF2EFE7)
+val Teal = Color(0xFF9ACBD0)
+val Sea = Color(0xFF48A6A7)
+val Cold = Color(0xFF006A71)
