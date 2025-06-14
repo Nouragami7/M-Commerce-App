823b5f5c42da2b3dccb9308fcc5d6838b7e6c081
alyaa149 <lohussien2@gmail.com>
2025-06-14 23:49:45 +0300

    create UI of profile info

--- Buyva/app/src/main/java/com/example/buyva/features/profile/view/ProfileScreen.kt
+++ Buyva/app/src/main/java/com/example/buyva/features/profile/view/ProfileScreen.kt
@@ -1,20 +1,126 @@
 package com.example.buyva.features.profile.view
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontStyle
+import com.example.buyva.ui.theme.Cold
+import com.example.buyva.ui.theme.Gray
+import com.example.buyva.ui.theme.Sea
+import com.example.buyva.ui.theme.Teal
 
 @Composable
 fun ProfileScreen() {
-    Box(
+    Column(
         modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .background(Gray)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "ProfileScreen", color = Color.Black)
+        Text(
+            text = "Buyva",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            color = Cold
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Box(
+            modifier = Modifier
+                .size(110.dp)
+                .clip(CircleShape)
+                .background(Teal),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = Icons.Default.Person,
+                contentDescription = "Profile",
+                tint = Color.White,
+                modifier = Modifier.size(64.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(
+            text = "Hello, folan",
+            fontStyle = FontStyle.Italic,
+            fontSize = 18.sp,
+            color = Cold
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            ProfileOption(Icons.Default.LocalShipping, "My Orders", Sea)
+            ProfileOption(Icons.Default.FavoriteBorder, "My Wishlist", Sea)
+            ProfileOption(Icons.Default.LocationOn, "Delivery Address", Sea)
+            ProfileOption(Icons.Default.Settings, "Settings", Sea)
+        }
+        Spacer(modifier = Modifier.height(30.dp))
+
+        OutlinedButton(
+            onClick = { },
+            colors = ButtonDefaults.outlinedButtonColors(contentColor = Cold),
+            border = BorderStroke(1.dp, Cold)
+        ) {
+            Icon(Icons.Default.ExitToApp, contentDescription = "Logout")
+            Spacer(Modifier.width(8.dp))
+            Text("Log Out", fontWeight = FontWeight.SemiBold)
+        }
+        Spacer(modifier = Modifier.height(40.dp))
+
+
+    }
+}
+@Composable
+fun ProfileOption(icon: ImageVector, title: String, iconColor: Color) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 9.dp)
+            .height(60.dp),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable { }
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = title,
+                tint = iconColor,
+                modifier = Modifier.size(28.dp)
+            )
+            Spacer(modifier = Modifier.width(20.dp))
+            Text(
+                text = title,
+                fontSize = 18.sp,
+                color = Color.Black,
+                fontWeight = FontWeight.Medium
+            )
+        }
     }
 }
