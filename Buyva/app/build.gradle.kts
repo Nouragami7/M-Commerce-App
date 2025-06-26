fc0fc94580eee515e54f7ff8abdd95c72ddfa77a
Youssef Abdelkader Allam <161586891+YoussefAllam02@users.noreply.github.com>
2025-06-26 23:38:24 +0300

    make authrepo test

--- Buyva/app/build.gradle.kts
+++ Buyva/app/build.gradle.kts
@@ -136,6 +136,10 @@ dependencies {
     testImplementation ("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation ("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     testImplementation ("org.junit.jupiter:junit-jupiter-params:5.8.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
+
+
 }
 
 apollo {
